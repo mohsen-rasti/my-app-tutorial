fa138fd5b05cae4bf2fc464dbbffba606c2db324
mohsen.rasti <mohsen.rasti@yahoo.com>
2021-10-01 22:20:49 +0330

    navbar

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,40 +1,32 @@
 import React from "react";
+import "../App.css";
+import logo from "../logo.svg";
+
 
 function Navbar() {
     return (
         <>
 <nav class="navbar navbar-expand-lg navbar-light bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand " href="#"><img src={logo} style={{width:'35px'}}></img></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider" /></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
-        </li>
-      </ul>
+      <ul class="navbar-nav m-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+            <a class="nav-link text-white text-uppercase active" aria-current="page" href="#">خانه&nbsp;<i class="fas fa-home"></i></a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link text-white text-uppercase ml-6 active" aria-current="page" href="#">اخبار&nbsp;<i class="fas fa-newspaper"></i></a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link text-white text-uppercase active" aria-current="page" href="#">ارتباط با ما&nbsp;<i class="fas fa-id-card"></i></a>
+            </li>
+       </ul>
       <form class="d-flex">
         {/* <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"> */}
-        <button class="btn btn-outline-success" type="submit">Search</button>
+        <button class="btn btn-outline-primary" type="submit">جستجو&nbsp;<i class="fas fa-search"></i></button>
       </form>
     </div>
   </div>
