7dbf1ab9cafea99b7df3c80ae78ced1006d24ade
mohsen.rasti <mohsen.rasti@yahoo.com>
2021-10-02 22:39:24 +0330

    style-component Footer style

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
   return (
     <>
 <Navbar />
+<br/>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe iure ratione praesentium quasi nostrum quod incidunt, odio quam repellendus eaque!
+<br/>
+Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo quia minima esse recusandae facere voluptate repudiandae ullam accusamus obcaecati in.
 <Footer />
     </>
   );
--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import "../App.css";
-
+import styled from "styled-components";
 
 function Footer() {
     return (
         <>
         
-<div className="main-footer">
+<FooterContainer className="main-footer">
+<div className="footer-middle">
     <div className="container">
         <div className="row">
            {/* Column 1 */}
@@ -43,10 +44,10 @@ function Footer() {
             <div className="col-md-3 col-sm-6">
                 <h4>lorem20</h4>
                 <ul className="list-unstyled">
+                    <li><a href="/">Lorem ++ </a></li>
                     <li>Lorem ++</li>
                     <li>Lorem ++</li>
-                    <li>Lorem ++</li>
-                    <li>Lorem ++</li>
+                    <li><a href="/">Lorem ++ </a></li>
                 </ul>
                 </div>    
         </div>
@@ -57,9 +58,29 @@ function Footer() {
           </p>
         </div>
     </div>
+   
 </div>
+</FooterContainer>
         </>
     )
 }
 
-export default Footer
+export default Footer;
+const FooterContainer=styled.footer`
+.footer-middle{
+background:var(--mainDark);
+padding-top:3rem;
+color:var(--mainWhite);
+}
+.footer-bottom{
+padding-top:3rem;
+padding-bottom:2rem;
+}
+ul li a{
+    color:var(--mainGray);
+}
+
+ul li a:hover{
+    color:var(--mainLightGray);
+}
+`;
