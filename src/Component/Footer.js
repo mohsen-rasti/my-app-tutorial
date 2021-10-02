5fcca07ee7f7b1445e6c4dfd85081838589bb443
mohsen.rasti <mohsen.rasti@yahoo.com>
2021-10-02 13:56:28 +0330

    change footer

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import "../App.css";
+
 
 function Footer() {
     return (
         <>
+        
 <div className="main-footer">
     <div className="container">
         <div className="row">
            {/* Column 1 */}
             <div className="col-md-3 col-sm-6">
-                <h4>lorem20</h4>
+                <h4>ارتباط با ما</h4>
                 <ul className="list-unstyled">
-                    <li>Lorem ++</li>
-                    <li>Lorem ++</li>
-                    <li>Lorem ++</li>
-                    <li>Lorem ++</li>
+                    <li ><a href="#"><i class="fab fa-instagram"></i> اینستاگرام</a></li>
+                    <li ><a href="#"><i class="fab fa-telegram"></i> Telegram </a></li>
+                    <li ><a href="#"><i class="fab fa-whatsapp"></i> WhatsApp</a></li>
+                    <li ><a href="#"><i class="fas fa-phone"></i> +98 917 310 9470</a></li>
                 </ul>
           </div>
            {/* Column 2 */}
