e71f4809286a7e870f704c280313c04564d0d8fc
mohsen.rasti <mohsen.rasti@yahoo.com>
2021-10-04 23:42:35 +0330

    edit details

--- src/Component/Pages/Details.js
+++ src/Component/Pages/Details.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { InfoConsumer } from "../contex";
+import styled from "styled-components";
 
 function Details() {
   return (
@@ -17,38 +18,67 @@ function Details() {
         } = data.detailInfo;
         return (
           <React.Fragment>
-            <div className="conteiner-fluid align-items-center">
+            <HeaderDetails className="container-fluid align-items-center" >
               <h1 className="display-1 font-weight-bold">{headerTitle}</h1>
-              <h4 className="display-f">{headerSubTitle}</h4>
+              <h4 className="display-5">{headerSubTitle}</h4>
               <p>{headerText}</p>
-            
-            {/* social Icons */}
-<div className="container mt-5">
-    <div className="row justify-contenet-center">
 
-<div className="col-2">   <i class="fab fa-facebook-f" ></i></div>
-<div className="col-2">   <i class="fab fa-twitter" ></i></div>
-<div className="col-2">   <i class="fab fa-google" ></i></div>
-<div className="col-2">   <i class="fab fa-reddit" ></i></div>
-<div className="col-2">   <i class="fab fa-whatsapp" ></i></div>
-<div className="col-2">   <i class="fab fa-telegram" ></i></div>
+              {/* social Icons */}
+              <div className="container mt-5" >
+                <div className="row justify-contenet-center">
+                  <div className="col-2">
+                  
+                    <i class="fab fa-facebook-f"></i>
+                  </div>
+                  <div className="col-2">
+                  
+                    <i class="fab fa-twitter"></i>
+                  </div>
+                  <div className="col-2">
+                  
+                    <i class="fab fa-google-plus"></i>
+                  </div>
+                  <div className="col-2">
+                  
+                    <i class="fab fa-reddit"></i>
+                  </div>
+                  <div className="col-2">
+                  
+                    <i class="fab fa-whatsapp"></i>
+                  </div>
+                  <div className="col-2">
+                  
+                    <i class="fab fa-telegram"></i>
+                  </div>
+                </div>
+              </div>
+            </HeaderDetails>
 
-    </div>
-</div>
 
-
-            </div>
-{/* nav Link */}
-
+{/* Nav Link */}
 <div className="container">
-    
-    <div className="nav nav-tabs">
-        {/* About Place Link */}
-        <li className="nav-item">
-            <a href="#aboutPlace" className="nav-link" role="tab" data-toggle="tab"></a>
-        </li>
-    </div>
+ 
+  <ul className="nav nav-tabs">
+  
+  {/* About Place Link */}   
+    <li className="nav item">
+      <a href="#aboutPlace" className="nav-link active" role="tab" data-toggle="tab">About</a>
+    </li>
+{/* Reviews Link */}   
+<li className="nav item">
+      <a href="#reviews" className="nav-link" role="tab" data-toggle="tab">Rewiews</a>
+    </li>
+{/* Map Link */}   
+<li className="nav item">
+      <a href="#maps" className="nav-link" role="tab" data-toggle="tab">Maps</a>
+    </li>
+
+  </ul>
+
 </div>
+            
+
+
           </React.Fragment>
         );
       }}
@@ -57,3 +87,49 @@ function Details() {
 }
 
 export default Details;
+
+const HeaderDetails = styled.header`
+  background: linear-gradient(rgba(109, 109, 109), rgba(255, 255, 255));
+  height: 100%;
+  text-transform: uppercase;
+  color: var(--mainWhite);
+  text-align: center;
+
+  h1 {
+    padding-top: 10%;
+    color: var(--mainDark);
+  }
+
+  h4 {
+    color: var(--maindark);
+  }
+
+  p {
+    padding-left: 10%;
+    padding-right: 10%;
+    margin-bottom: 10%;
+    color: var(--maindark);
+  }
+
+  i{
+    font-size:1.875rem;
+    color:var(--mainDark);
+    
+  }
+
+  i:hover{
+    color:var(--mainBlue);
+    cursor:pointer;
+  }
+
+  .nav-item{
+    height:18.75rem;
+
+  }
+
+  @media(max-width:760px){
+    h1,h2{
+      color:var(--mainWhite);
+    }
+  }
+`;
